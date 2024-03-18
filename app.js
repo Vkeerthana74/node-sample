edf075d5ec73353d2bc1af223c159a79efd0636f
nithiishkumarb <nithiishkumarb.gmail.com>
2024-03-18 17:51:16 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const express=require("express")
+const app=express();
+
+
+app.get('/',(req,res)=>{
+    res.send("Hi world is this sample testing response")
+})
+app.listen(9000,()=>{
+    console.log("Server is running in port 9000")
+})
