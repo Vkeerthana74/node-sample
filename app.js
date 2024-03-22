8030a27248d1dc96457f60c184a9a32df12e8ba7
Nithish kumar B <134282957+nithiishkumarb@users.noreply.github.com>
2024-03-22 10:27:26 +0000

    alter

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("Hi world is this sample testing response");
 });
-app.listen(8080, () => {
+app.listen(9000, () => {
   console.log("Server is running in port 9000");
 });
